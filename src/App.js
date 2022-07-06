bcf79045552a5b62589b599bbcd0e3c2562d1dad
fstawicki <stawickifilip1@gmail.com>
2022-07-07 00:35:43 +0200

    added props to headline component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,26 @@ import Headline from './components/Headline';
 import Carrousel from './components/Carrousel';
 import C2 from './components/C2';
 
+
+const headlineData = {
+  exterior: {
+    headlineTitle: 'Exterior.',
+    headlineParagraph: 'The A-Class as a personal, individual item – every customer is able to comprehensively individualise their vehicle and configure it to be more classic or lifestyle-oriented. The design and equipment lines define the direction here. Nevertheless, they provide plenty of scope for further special touches.'
+  },
+  interior: {
+    headlineTitle: 'Interior.',
+    headlineParagraph: 'The interior combines sportiness with an even more generous sense of space. At the same time, it meets the highest of aesthetic standards and adds this to a versatility of design for people with creative flair.'
+  },
+  technology: {
+    headlineTitle: 'Technology.',
+    headlineParagraph: ''
+  },
+  safety: {
+    headlineTitle: 'Safety.',
+    headlineParagraph: 'Mercedes-Benz Intelligent Drive combines safety and convenience innovations. With these intelligent systems, Mercedes-Benz is not only setting standards in automotive engineering, but is also taking another important step forward on the road to accident-free and autonomous driving.'
+  }
+}
+
 function App() {
   return (
     <div className="App">
@@ -16,10 +36,18 @@ function App() {
           <HeroPage/>
           <SecNavbar />
         </Wrapper>
-        <Headline />
-        {/* <C2 /> */}
-
+        <Headline title={headlineData.exterior.headlineTitle} paragraph={headlineData.exterior.headlineParagraph} />
         <Carrousel />
+        {/* element ze zdjęciem */}
+        <Headline title={headlineData.interior.headlineTitle} paragraph={headlineData.interior.headlineParagraph} />
+        {/* karuzela
+        element ze zdjęciem */}
+        <Headline title={headlineData.technology.headlineTitle} paragraph={headlineData.technology.headlineParagraph} />
+        {/* karuzela
+        element ze zdjęciem       */}
+        <Headline title={headlineData.safety.headlineTitle} paragraph={headlineData.safety.headlineParagraph} />
+        {/* karuzela
+        element ze zdjęciem   */}
 
       <Container />
     </div>
--- src/components/Headline.jsx
+++ src/components/Headline.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './Headline.css';
 
-const Headline = () => {
+const Headline = (props) => {
   return (
     <div className="Headline">
         <div className="Headline__container">
             
-            <h2 className='Headline__container__title'>Exterior.</h2>
-            <p className='Headline__container__text'>The A-Class as a personal, individual item – every customer is able to comprehensively individualise their vehicle and configure it to be more classic or lifestyle-oriented. The design and equipment lines define the direction here. Nevertheless, they provide plenty of scope for further special touches.</p>
+            <h2 className='Headline__container__title'>{props.title}</h2>
+            <p className='Headline__container__text'>{props.paragraph}</p>
         </div>
     </div>
   )
