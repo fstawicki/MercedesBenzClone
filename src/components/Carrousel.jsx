9615ac86ac8e93edd79a41d76d0a8323d2b4c3f3
fstawicki <stawickifilip1@gmail.com>
2022-07-06 22:46:24 +0200

    different images for different slides added

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -13,6 +13,9 @@ import { Pagination } from "swiper";
 
 import styled from 'styled-components';
 
+import c1 from '../images/mercedesphotos/carrousels/carrousel1-1.jpg';
+import c2 from '../images/mercedesphotos/carrousels/carrousel1-2.jpg';
+import c3 from '../images/mercedesphotos/carrousels/carrousel1-3.jpg';
 
 // export const SlideWrapper = styled.div`
 
@@ -38,21 +41,24 @@ const Carrousel = () => {
   //     alt: 'mercedes-rear'
   //   },
   // ];
-
+  // backgroundImage: `url(${Background})`
 
   //DUMMY FOR NOW
   const slides_texts = [
     {
       title: 'The front-end design.',
-      paragraph: 'The striking front section exudes more than ever an air of self-confidence and appeal at the same. The typical energetic look and thus the recognition value are underscored by the new generation of headlamps – with an impressive high-tech appearance.'
+      paragraph: 'The striking front section exudes more than ever an air of self-confidence and appeal at the same. The typical energetic look and thus the recognition value are underscored by the new generation of headlamps – with an impressive high-tech appearance.',
+      imageURL: c1
     },
     {
       title: 'The side design.',
-      paragraph: 'The surface-oriented design gives the side view of the A-Class Saloon its special aura. Very few recesses in the stretched bodyshell – a puristic design that arouses emotions. With the elegant roofline as a further highlight.'
+      paragraph: 'The surface-oriented design gives the side view of the A-Class Saloon its special aura. Very few recesses in the stretched bodyshell – a puristic design that arouses emotions. With the elegant roofline as a further highlight.',
+      imageURL: c2
     },
     {
       title: 'The rear-end design.',
-      paragraph: 'The two-part tail lamps underscore the clear rear end that emphasises width. The rear reflectors were relocated to the modular two-part rear bumper. Depending on the selected equipment, this rear bumper is available with a black diffuser or with chrome trim strip and chrome tailpipe tips.'
+      paragraph: 'The two-part tail lamps underscore the clear rear end that emphasises width. The rear reflectors were relocated to the modular two-part rear bumper. Depending on the selected equipment, this rear bumper is available with a black diffuser or with chrome trim strip and chrome tailpipe tips.',
+      imageURL: c3
     },
   ]
 
@@ -71,17 +77,20 @@ const Carrousel = () => {
 
     const slides = [];
 
-  //i < 5 zmienic 5 pozniej na props
+  //i < 5 zmienic 5 pozniej na props i i=0 tez na np 4 jak zaczniemy 2 sekcje karuzeli
   for(let i=0; i<3; i+=1){
+
+    let wrapperStyle = {backgroundImage: `url(${slides_texts[i].imageURL})`};
+
     slides.push(
-      <SwiperSlide className='SwiperSlide' key={`slide-${i}`}>
+      <SwiperSlide className='SwiperSlide' key={`slide-${i}`}  style={wrapperStyle}>
         
         {/* <img className='SwiperSlide__img' src={require(`../images/mercedesphotos/carrousels/carrousel1-${i+1}.jpg`)} alt='mercedes image'  /> */}
         <div className='SwiperSlide__wrapper'>
-        <div className='SwiperSlide__text'>
-          <p className='SwiperSlide__text__title'>{slides_texts[i].title}</p>
-          <p className='SwiperSlide__text__paragraph'>{slides_texts[i].paragraph}</p>
-        </div>
+          <div className='SwiperSlide__text'>
+            <p className='SwiperSlide__text__title'>{slides_texts[i].title}</p>
+            <p className='SwiperSlide__text__paragraph'>{slides_texts[i].paragraph}</p>
+          </div>
         </div>
       </SwiperSlide>
     )
