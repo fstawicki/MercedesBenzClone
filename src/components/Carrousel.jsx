540a815a2f2beb903fa957aaebfff14f0a8e3fc9
fstawicki <stawickifilip1@gmail.com>
2022-07-08 21:30:16 +0200

    working on responsiveness

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -46,17 +46,17 @@ const Carrousel = () => {
   //DUMMY FOR NOW
   const slides_texts = [
     {
-      title: 'The front-end design.',
+      title: 'The front-end design',
       paragraph: 'The striking front section exudes more than ever an air of self-confidence and appeal at the same. The typical energetic look and thus the recognition value are underscored by the new generation of headlamps – with an impressive high-tech appearance.',
       imageURL: c1
     },
     {
-      title: 'The side design.',
+      title: 'The side design',
       paragraph: 'The surface-oriented design gives the side view of the A-Class Saloon its special aura. Very few recesses in the stretched bodyshell – a puristic design that arouses emotions. With the elegant roofline as a further highlight.',
       imageURL: c2
     },
     {
-      title: 'The rear-end design.',
+      title: 'The rear-end design',
       paragraph: 'The two-part tail lamps underscore the clear rear end that emphasises width. The rear reflectors were relocated to the modular two-part rear bumper. Depending on the selected equipment, this rear bumper is available with a black diffuser or with chrome trim strip and chrome tailpipe tips.',
       imageURL: c3
     },
