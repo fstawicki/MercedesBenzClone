e33dd86f072a0aecbb376f986ce3b25b76b4672e
fstawicki <stawickifilip1@gmail.com>
2022-07-11 20:50:40 +0200

    working on headlineImage component css

--- src/components/ImageHeadline.jsx
+++ src/components/ImageHeadline.jsx
@@ -7,14 +7,19 @@ const ImageHeadline = (props) => {
 
   return (
     <div className='ImageHeadline'>
-        <div className='ImageHeadline__imagediv' style={imagedivStyle}></div>
-        <div className='ImageHeadline__text'>
-            <p className='ImageHeadline__text__paragraph'>{props.imgheadlineData.headlineParagraph}</p>
-            <h2 className='ImageHeadline__text__title'>{props.imgheadlineData.headlineTitle}</h2>
+      <div className="ImageHeadline__container">
+        <div className='ImageHeadline__container__imagediv'>
+          <img src={props.imgheadlineData.imageURL} alt="" />
+
         </div>
-        <div className='ImageHeadline__arrow'>
-            arrow
+        <div className='ImageHeadline__container__text'>
+            <p className='ImageHeadline__container__text__paragraph'>{props.imgheadlineData.headlineParagraph}</p>
+            <h2 className='ImageHeadline__container__text__title'>{props.imgheadlineData.headlineTitle}</h2>
         </div>
+        <div className='ImageHeadline__container__arrow'>
+            >
+        </div>
+      </div>
     </div>
   )
 }
