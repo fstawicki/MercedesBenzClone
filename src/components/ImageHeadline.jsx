8cc4f0bdeec42d031f51bf80a9fd6ea7d403a3de
fstawicki <stawickifilip1@gmail.com>
2022-07-11 21:17:43 +0200

    added headlineImage component responsiveness according to Headline component

--- src/components/ImageHeadline.jsx
+++ src/components/ImageHeadline.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import './ImageHeadline.css';
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
+
+
 const ImageHeadline = (props) => {
 
   let imagedivStyle = {backgroundImage: `url(${props.imgheadlineData.imageURL})`};
@@ -10,14 +14,13 @@ const ImageHeadline = (props) => {
       <div className="ImageHeadline__container">
         <div className='ImageHeadline__container__imagediv'>
           <img src={props.imgheadlineData.imageURL} alt="" />
-
         </div>
         <div className='ImageHeadline__container__text'>
             <p className='ImageHeadline__container__text__paragraph'>{props.imgheadlineData.headlineParagraph}</p>
             <h2 className='ImageHeadline__container__text__title'>{props.imgheadlineData.headlineTitle}</h2>
         </div>
-        <div className='ImageHeadline__container__arrow'>
-            >
+        <div className='ImageHeadline__container__arrowdiv'>
+          <FontAwesomeIcon className='ImageHeadline__container__arrowdiv__arrow' icon={faAngleRight} />
         </div>
       </div>
     </div>
